c556ec857eeca1dce6ce4ba9004011ba67376e0e
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-27 22:14:14 +0900

    [Finish]books#index,users#showページ　いいね週間ランキング順実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b| 
+      b.favorites.where(created_at: from...to).size <=> 
+      a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
   end
 
